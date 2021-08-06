52597cb8c66bfd58f62f3edbb39a20aa383a9162
Aman <amantiwari8861@gmail.com>
2021-08-06 18:12:54 +0530

    looping done

---
+++ if1.java
@@ -0,0 +1,79 @@
+public class if1 {
+    public static void main(String[] args) {
+        
+        // int num=45;
+
+        // if(num>10)
+        // {
+        //     System.out.println("num is greater than 10");
+        // }
+        // else if(num==18)
+        // {
+        //     System.out.println("the num is equal to 18");
+        // }
+        // else
+        // {
+        //     System.out.println("num is not greater than 10");
+        // }
+        // if (num>18) {
+        //     System.out.println("greater than 18");
+        //     if (num==45) {
+        //         System.out.println("45 yrs");
+
+        //     }
+        //     else
+        //     {
+        //         System.out.println("not equal to 45");
+        //     }
+        // }
+            // char operator='+';
+
+            // switch (operator) {
+            //     case '+':
+            //         System.out.println("plus");
+            //         break;
+            //     case '*':
+            //         System.out.println("multi");
+            //         break;
+            //     case '-':
+            //         System.out.println("sub");
+            //         break;
+            //     default:
+            //         System.out.println("not valid!");
+            //         break;
+            // }
+            // for (int i = 0; i < 10; i++) {
+            //     System.out.println(i);
+            // }
+            // int i;
+            // for (i = 0; i < 10; i++)
+            // {
+            //     System.out.println(i);
+            // }
+            // for(;;)
+            // {
+            //     System.out.println("infinite for loop");
+            // }
+            // while (true) {
+            //     System.out.println("infinite while loop");
+            // }
+            // int i=0;
+            // while (i<10) {
+            //     System.out.println(i);
+            //     i++;
+            // }
+            // int i=20;
+            // do {
+            //         System.out.println(i);
+            //         i++;
+            // } while (i<10);
+            for (int i = 0; i < 5; i++) 
+            {
+                for (int j = 0; j < 5; j++) 
+                {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+    }
+}
---
+++ operators.java
@@ -0,0 +1,15 @@
+
+public class operators {
+
+    public static void main(String[] args) {
+        
+        int num=10;
+        //syntax :  condition ? if true : if false ;
+        int result= num<40 ? 30 : 10 ; // ? 
+        float pi=3.14589f;
+        System.out.printf("the result is %d and %.2f ",result,pi);
+
+        
+
+    }
+}
