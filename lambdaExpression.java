c3cb7ba4094e87c96d63ff7cbaaec7e176975dc4
Aman <amantiwari8861@gmail.com>
2021-12-03 16:00:26 +0530

    collection finish

--- lambdaExpression.java
+++ lambdaExpression.java
@@ -1,6 +1,5 @@
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Collections;
 
 interface DemoInterface1 { //To implement the lambda expression with no arguments
     void noArguments();
