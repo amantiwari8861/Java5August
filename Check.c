fa1163f8cb86707d7c409fd94c7553b0b9d74e21
Aman Tiwari <36691571+amantiwari8861@users.noreply.github.com>
2023-06-11 15:29:06 +0530

    minor change

--- Check.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-
-int main()
-{
-    double d=123.123456789834;
-    printf(" %lf ",d);
-    return 0;
-}
--- newInterface.java
+++ newInterface.java
@@ -17,38 +17,34 @@
  * @author Aman Tiwari
  */
 
- // Java 9 program to illustrate
+// Java 9 program to illustrate
 // private methods in interfaces
- interface TempI {
+interface TempI {
 
 	public abstract void mul(int a, int b);
 
-public default void add(int a, int b)
-	{
-        // private method inside default method
+	public default void add(int a, int b) {
+		// private method inside default method
 		sub(a, b);
 
-        // static method inside other non-static method
+		// static method inside other non-static method
 		div(a, b);
 		System.out.print("Answer by Default method = ");
 		System.out.println(a + b);
 	}
 
-	public static void mod(int a, int b)
-	{
+	public static void mod(int a, int b) {
 		div(a, b); // static method inside other static method
 		System.out.print("Answer by Static method = ");
 		System.out.println(a % b);
 	}
 
-	private void sub(int a, int b)
-	{
+	private void sub(int a, int b) {
 		System.out.print("Answer by Private method = ");
 		System.out.println(a - b);
 	}
 
-	private static void div(int a, int b)
-	{
+	private static void div(int a, int b) {
 		System.out.print("Answer by Private static method = ");
 		System.out.println(a / b);
 	}
@@ -56,18 +52,16 @@ public default void add(int a, int b)
 
 class Temp implements TempI {
 
-	public void mul(int a, int b)
-	{
+	public void mul(int a, int b) {
 		System.out.print("Answer by Abstract method = ");
 		System.out.println(a * b);
 	}
 
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		TempI in = new Temp();
 		in.mul(2, 3);
 		in.add(6, 2);
 		TempI.mod(5, 3);
-        // TempI.div(20,10);
+		// TempI.div(20,10);
 	}
 }
